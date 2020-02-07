bf9fc718b851c3ef1907402e787de6994f3ab5f1
John Cyphert <cyphertjohn@gmail.com>
2020-02-07 15:32:23 -0600

    Started to add NRA capability

---
+++ src/boolEval.ml
@@ -0,0 +1,85 @@
+module Logger = Log
+
+module VarMap = Map.Make(String)
+
+module Make (A : sig val eval_base : float VarMap.t -> Z3.Expr.expr -> bool end) = struct
+
+  let rec eval assignment expr =
+    if Z3.Boolean.is_true expr || Z3.Expr.to_string expr = "true" then (
+      true)
+    else if Z3.Boolean.is_false expr || Z3.Expr.to_string expr = "false" then (
+      false)
+    else if Z3.Boolean.is_or expr then (
+      List.fold_left (||) false (List.map (eval assignment) (Z3.Expr.get_args expr)))
+    else if Z3.Boolean.is_and expr then (
+      List.fold_left (&&) true (List.map (eval assignment) (Z3.Expr.get_args expr)))
+    else if Z3.Boolean.is_not expr then (
+      not (eval assignment (List.nth (Z3.Expr.get_args expr) 0)))
+    else if Z3.Boolean.is_eq expr then (
+      let (left, right) = (eval assignment (List.nth (Z3.Expr.get_args expr) 0), eval assignment (List.nth (Z3.Expr.get_args expr) 1)) in
+      left = right)
+    else if Z3.Boolean.is_iff expr then (
+      let (left, right) = (eval assignment (List.nth (Z3.Expr.get_args expr) 0), eval assignment (List.nth (Z3.Expr.get_args expr) 1)) in
+      left = right)
+    else if Z3.Boolean.is_implies expr then (
+      let (left, right) = (eval assignment (List.nth (Z3.Expr.get_args expr) 0), eval assignment (List.nth (Z3.Expr.get_args expr) 1)) in
+      not left || right)
+    else if Z3.Boolean.is_xor expr then (
+      let (left, right) = (eval assignment (List.nth (Z3.Expr.get_args expr) 0), eval assignment (List.nth (Z3.Expr.get_args expr) 1)) in
+      (left || right) && not (left && right))
+    else A.eval_base assignment expr
+
+end
+
+module Boolean = struct
+  let eval_base assignment expr = 
+    if Z3.Expr.is_const expr then (
+      let name = (Z3.Expr.to_string expr) in
+      (VarMap.find name assignment) > 0.)
+    else failwith ("Unsupported function in boolean: " ^ (Z3.Expr.to_string expr))
+end
+
+module Arithmetic = struct
+
+  let rec eval_function assignment expr = 
+    if Z3.Arithmetic.is_arithmetic_numeral expr then (
+      float_of_string (Z3.Expr.to_string expr))
+    else if Z3.Expr.is_const expr then (
+      let name = (Z3.Expr.to_string expr) in
+      VarMap.find name assignment)
+    else if Z3.Arithmetic.is_add expr then (
+      let children = List.map (eval_function assignment) (Z3.Expr.get_args expr) in
+      List.fold_left (+.) (List.hd children) (List.tl children))
+    else if Z3.Arithmetic.is_mul expr then (
+      let children = List.map (eval_function assignment) (Z3.Expr.get_args expr) in
+      List.fold_left ( *. ) (List.hd children) (List.tl children))
+    else if Z3.Arithmetic.is_uminus expr then (
+      let child = (eval_function assignment) (List.nth (Z3.Expr.get_args expr) 0) in
+      (-1.) *. child)
+    else if Z3.Arithmetic.is_sub expr then (
+      let children = List.map (eval_function assignment) (Z3.Expr.get_args expr) in
+      (List.nth children 0) -. (List.nth children 1))
+    else if Z3.Arithmetic.is_div expr then (
+      let children = List.map (eval_function assignment) (Z3.Expr.get_args expr) in
+      (List.nth children 0) /. (List.nth children 1))
+    else failwith ("Unsupported base function: " ^ (Z3.Expr.to_string expr))    
+
+  let eval_base assignment expr = 
+    if Z3.Arithmetic.is_ge expr then (
+      let (left, right) = (eval_function assignment (List.nth (Z3.Expr.get_args expr) 0), eval_function assignment (List.nth (Z3.Expr.get_args expr) 1)) in
+      left >= right)
+    else if Z3.Arithmetic.is_gt expr then (
+      let (left, right) = (eval_function assignment (List.nth (Z3.Expr.get_args expr) 0), eval_function assignment (List.nth (Z3.Expr.get_args expr) 1)) in
+      left > right)
+    else if Z3.Arithmetic.is_le expr then (
+      let (left, right) = (eval_function assignment (List.nth (Z3.Expr.get_args expr) 0), eval_function assignment (List.nth (Z3.Expr.get_args expr) 1)) in
+      left <= right)
+    else if Z3.Arithmetic.is_lt expr then (
+      let (left, right) = (eval_function assignment (List.nth (Z3.Expr.get_args expr) 0), eval_function assignment (List.nth (Z3.Expr.get_args expr) 1)) in
+      left < right)
+    else failwith ("Unrecognized arithmetic predicate: " ^ (Z3.Expr.to_string expr))
+end
+
+module BoolEval = Make(Boolean)
+
+module ArithEval = Make(Arithmetic)
--- src/rsat.ml
+++ src/rsat.ml
@@ -13,25 +13,11 @@ let rec read_file in_channel res =
     res
   ;;
 
-let rec print_sorts expr = 
-  Logger.log ((Z3.Sort.to_string (Z3.Expr.get_sort expr)) ^ "\n");
-  if (Z3.Expr.get_num_args expr) = 0 then ()
-  else 
-    List.iter print_sorts (Z3.Expr.get_args expr)
-  ;;
-
 let parse query = 
   let ctx = Z3.mk_context [] in
   let ast_vec = Logger.log_time "Parsing" (Z3.SMT.parse_smtlib2_string ctx query [] [] []) [] in
   (ctx, Z3.Boolean.mk_and ctx (Z3.AST.ASTVector.to_expr_list ast_vec))
-  ;;
-
-(*module DL2 = Loss.Make ()
-module DL2List = LossList.Make ()
-module SL = Search.Make(DL2)
-module SLL = Search.Make(DL2List)
-module LPLL = Search.Sum(SL)(SLL)*)
-  
+  ;;  
 
 module Rfunction = Translate.Make(Rfun.Make(GraphAD.Make ()))
 module Loss = Translateloss.Make(Loss.Make(GraphAD.Make ()))
@@ -45,20 +31,6 @@ type status =
 
 let iterations = ref 6
 
-(*let r_fun expr ctx =
-  let res = 
-    if (Z3.Expr.to_string expr = "false") then (Logger.log ("unsat\n"); UNSAT) (*Z3 bug. Z3 says false is a variable sometimes*)
-    else (
-      let (new_form, assign) = Optimize.remove_triv expr ctx in
-      Logger.log ("Optimized formula: " ^ (Z3.Expr.to_string new_form) ^ "\n") ~level:`trace;
-      let search_res = Logger.log_time "Search" (Search.search new_form assign ~iter:!iterations) () in
-      (match search_res with
-        | None -> Logger.log ("unknown\n"); UNKNOWN
-        | Some x -> Logger.log ("sat\n"); SAT x
-      )
-    ) in
-  res
-  ;;*)
 
 let r_fun expr ctx =
   let res = 
@@ -77,56 +49,9 @@ let r_fun expr ctx =
   ;;
 
 
-let rec make_sentence ctx assign expr =
-  if Z3.Boolean.is_true expr then (
-    Z3.Boolean.mk_true ctx)
-  else if Z3.Boolean.is_false expr then (
-    Z3.Boolean.mk_false ctx)
-  else if Z3.Boolean.is_and expr then (
-    let subforms = List.map (fun ex -> make_sentence ctx assign ex) (Z3.Expr.get_args expr) in
-    Z3.Boolean.mk_and ctx subforms) 
-  else if Z3.Boolean.is_or expr then (
-    let subforms = List.map (fun ex -> make_sentence ctx assign ex) (Z3.Expr.get_args expr) in
-    Z3.Boolean.mk_or ctx subforms)
-  else if Z3.Boolean.is_not expr then (
-    let subform = make_sentence ctx assign (List.nth (Z3.Expr.get_args expr) 0) in
-    Z3.Boolean.mk_not ctx subform)
-  else if Z3.Expr.is_const expr then (
-    let name = Z3.Expr.to_string expr in
-    let v = VarMap.find name assign in
-    if v > 0.5 then
-      Z3.Boolean.mk_true ctx
-    else 
-      Z3.Boolean.mk_false ctx)
-  else if Z3.Boolean.is_eq expr then (
-    let subforms = List.map (fun ex -> make_sentence ctx assign ex) (Z3.Expr.get_args expr) in
-    Z3.Boolean.mk_eq ctx (List.nth subforms 0) (List.nth subforms 1))
-  else ( failwith (Z3.Expr.to_string expr));;
-
-let r_test ctx assign expr =
-  match assign with
-  | UNSAT -> "UNSAT"
-  | UNKNOWN -> "OKAY"
-  | SAT assig ->
-    if Z3.Expr.to_string (Z3.Expr.simplify (make_sentence ctx assig expr) None) = "true" then "PASS"
-    else "FAIL"
-
-let test_rewrite old_ctx aufbv new_ctx sat =
-  let old_solver = Z3.Solver.mk_solver old_ctx None in
-  let new_solver = Z3.Solver.mk_solver new_ctx None in
-  if (Z3.Solver.check old_solver [aufbv]) = (Z3.Solver.check new_solver [sat]) then "PASS"
-  else "FAIL"
-
 let log_out_file = ref false;;
-let test_assign = ref false;;
 let test_translate = ref false;;
 let no_array_theory = ref false;;
-
-let test arg = 
-  match arg with
-  | "rewrite" -> test_translate := true
-  | "assign" -> test_assign := true
-  | _ -> failwith "Unknown testing argument"
 
 let setOut fileName = 
   log_out_file := true;
@@ -142,7 +67,6 @@ let sat_file in_file_name =
   in
   let (new_ctx, sat) = Logger.log_time "SMT2SAT" bit_blast () in
   let res = r_fun sat new_ctx in
-  if !test_assign then Logger.log ((r_test new_ctx res sat) ^ "\n");
   close_in ic;
   if (!log_out_file) then close_out !Logger.chan
   else ()
@@ -151,11 +75,9 @@ let sat_file in_file_name =
 let register () = 
   let speclist = [("-o", Arg.String setOut, "Set an output file"); 
                   ("-v", Arg.String Logger.set_level, "Set versbosity [trace | debug | always]");
-                  ("-test", Arg.String test, "Test functionality [rewrite | assign]");
                   ("-time", Arg.Set Logger.log_times, "Log execution times");
                   ("-i", Arg.Set_int iterations, "The number of search iterations. Default is 6");
                   ("-no_array", Arg.Set no_array_theory, "The input file does not contain array terms")] in
-                  (*("-wen_list", Arg.Clear LPLL.use_left, "Use a Wengert list")] in*)
   let usage = "rsat.native <smt2-file>" in
   Arg.parse speclist sat_file usage
 
